b5a24c56e25a3042957ed6ecb97d9bc344f05203
Jon Lambson <jon@crowdengine.com>
2017-05-18 00:23:04 -0600

    pdf

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   get 'static_pages/portfolio'
-
   get 'static_pages/coming_soon'
-
   get 'static_pages/contact'
+  get 'static_pages/resume'
 
   devise_for :users
   root 'contacts#index'
